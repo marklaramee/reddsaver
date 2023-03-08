86f5657628582f7690c6b3cefefc56012e8c8d18
Mark Laramee <marklaramee@gmail.com>
2023-03-07 23:21:32 -0800

    Squashed commit of the following: more infrastructure
    commit 6685a2d7b8dfa56f3f0ec55982cdeeff81e1d1f4
    Author: Mark Laramee <marklaramee@gmail.com>
    Date:   Tue Mar 7 23:20:56 2023 -0800
    
        added comment
    
    commit 5238c448063028ec49fc1cbfd6cba1e9658ee956
    Author: Mark Laramee <mlaramee@rccl.com>
    Date:   Tue Mar 7 23:17:52 2023 -0800
    
        added logging
    
    commit 455493ba96d67da9caf11bee60b5107169745a08
    Author: Mark Laramee <mlaramee@rccl.com>
    Date:   Tue Mar 7 22:52:28 2023 -0800
    
        moved initial token acquisition to viewmodel
    
    commit d5c6ca634c10220bf458aab7e503dc7269960706
    Author: Mark Laramee <mlaramee@rccl.com>
    Date:   Tue Mar 7 22:39:33 2023 -0800
    
        adding tokenmanager

--- reddsaver/OAuth/ViewModel/OAuthViewModel.swift
+++ reddsaver/OAuth/ViewModel/OAuthViewModel.swift
@@ -13,11 +13,6 @@ class OAuthViewModel {
     var redirectPath = "reddsaver://oauth-callback"
     
     lazy var validationString = String.random(size: 42)
-        
-    var oAuthToken: String? {
-        let token: String? = UserStorage.shared.readGlobalValue(forKey: .accessToken)
-        return token
-    }
     
     // TODO: cleanup
     var clientId: String? {
@@ -25,11 +20,84 @@ class OAuthViewModel {
         return clientId
     }
     
-    func saveTokens(response: RedditResponse) {
-        print("ML: acccess_token \(response.access_token)")
-        print("ML: refresh_token \(response.refresh_token)")
-        UserStorage.shared.saveGlobalValue(forKey: .accessToken, value: response.access_token)
-        UserStorage.shared.saveGlobalValue(forKey: .refreshToken, value: response.refresh_token)
+    // https://github.com/reddit-archive/reddit/wiki/OAuth2#retrieving-the-access-token
+    /*
+     return completion success or failure
+     */
+    func getAccessToken(_ code: String, completion: @escaping (Bool) -> Void) {
+        // https://stackoverflow.com/a/75383148/641854 trim code string
+        let cleanCode = code.trimmingCharacters(in: CharacterSet(charactersIn: "#_"))
+        
+        guard let clientId = clientId else {
+            ReddLogger.shared.log(level: .error, message: "api key unavailable", category: .oAuth)
+            completion(false)
+            return
+
+        }
+
+        guard let url = URL(string: "https://www.reddit.com/api/v1/access_token") else {
+            ReddLogger.shared.log(level: .error, message: "url unavailable", category: .oAuth)
+            completion(false)
+            return
+        }
+        
+        var request = URLRequest(url: url)
+        request.httpMethod = "POST"
+        request.addValue("application/x-www-form-urlencoded", forHTTPHeaderField: "Content-Type")
+
+        let payload = "grant_type=authorization_code&code=\(cleanCode)&redirect_uri=\(redirectPath)"
+        request.httpBody = payload.data(using: .utf8)
+
+        // HTTP Basic authentication
+        let username = clientId
+        let password = ""
+        let loginData = "\(username):\(password)".data(using: .utf8)!
+        let base64LoginData = loginData.base64EncodedString()
+        request.setValue("Basic \(base64LoginData)", forHTTPHeaderField: "Authorization")
+
+        let session = URLSession.shared
+        let task = session.dataTask(with: request) { (data, response, error) in
+            if let sessionError = error {
+                ReddLogger.shared.log(level: .error, message: "Session error: \(sessionError).", category: .oAuth)
+                completion(false)
+                return
+            }
+            
+            guard let httpResponse = response as? HTTPURLResponse else {
+                ReddLogger.shared.log(level: .error, message: "Response was not httpResponse.", category: .oAuth)
+                completion(false)
+                return
+            }
+            
+            // TODO:
+            guard (200...299).contains(httpResponse.statusCode) else {
+                ReddLogger.shared.log(level: .error, message: "HTTP response code is not in the 2xx range \(httpResponse.statusCode).", category: .oAuth)
+                completion(false)
+                return
+            }
+            
+            guard let responseData = data else {
+                ReddLogger.shared.log(level: .error, message: "No response data.", category: .oAuth)
+                completion(false)
+                return
+            }
+            
+            do {
+                let redditResponse = try JSONDecoder().decode(RedditResponse.self, from: responseData)
+                // TODO: validate?
+                TokenManager.shared.saveTokens(response: redditResponse)
+                DispatchQueue.main.async {
+                    completion(true)
+                }
+            } catch {
+                // TODO: vzlidate this
+                ReddLogger.shared.log(level: .error, message: "\(error)", category: .oAuth)
+                completion(false)
+            }
+        }
+
+        // Start the network request
+        task.resume()
     }
     
 }
